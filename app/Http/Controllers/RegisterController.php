34b85a3694a3b445129f458586bde2be87706252
zlkhyr <zakhyar38@gmail.com>
2023-04-10 00:05:17 +0700

    hashing password

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 class RegisterController extends Controller
 {
     public function index()
@@ -15,13 +16,17 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
+        $validatedData = $request->validate([
             'name' => ['required', 'max:255'],
             'username' => ['required', 'min:3', 'max:255', 'unique:users'],
             'email' => ['required', 'email', 'unique:users'],
             'password' => ['required', 'min:5', 'max:255']
         ]);
 
-        dd('berhasil');
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+
+        return redirect('/');
     }
 }
