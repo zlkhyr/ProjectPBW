61b295acc2af818155b47722c3bf0f655829c3a9
zlkhyr <zakhyar38@gmail.com>
2023-05-08 00:01:43 +0700

    fix, search tida responsif

--- resources/views/componen/search.blade.php
+++ resources/views/componen/search.blade.php
@@ -1,4 +1,4 @@
 <form class="d-flex mb-3 mt-3" role="search">
-    <input class="form-control" type="search" placeholder="Search" aria-label="Search" style="border-top-right-radius: 0px; border-bottom-right-radius:0px; width: 800px; display:flex">
+    <input class="form-control" type="search" placeholder="Search" aria-label="Search" style="border-top-right-radius: 0px; border-bottom-right-radius:0px; width: 100%; display:flex">
     <button class="btn btn-outline-success" type="submit" style="border-top-left-radius: 0px; border-bottom-left-radius:0px;">Search</button>
 </form>
